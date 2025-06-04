7fe65e245ceee48ab4a96233299ae43c9a1effec
leo.wanty <wanty.leo@gmail.com>
2025-06-04 13:22:36 +0200

    feat(utils): add function for generating Fandom page URLs
    Introduce `get_fandom_page_url` to build URLs for specific pages on Fandom wikis.
    Refactor existing code to utilize the helper `_get_fandom_base_url`.

--- src/knowledge_base/utils/url.py
+++ src/knowledge_base/utils/url.py
@@ -1,17 +1,38 @@
 import urllib.parse
 
+
+def _get_fandom_base_url(fandom_url: str) -> str:
+    parsed_url = urllib.parse.urlparse(fandom_url)
+    return f"{parsed_url.scheme}://{parsed_url.netloc}"
+
+
 def get_fandom_statistics_page_url(fandom_url: str) -> str:
     """
     Constructs the URL for the Special:Statistics page of a Fandom wiki.
 
     Args:
         fandom_url: The base URL of the Fandom wiki
-                    (e.g., "https://asimov.fandom.com/wiki/Asimov_Wiki" or "https://asimov.fandom.com/").
+            (e.g., "https://asimov.fandom.com/wiki/Asimov_Wiki" or "https://asimov.fandom.com/").
 
     Returns:
         The URL of the Special:Statistics page.
     """
-    parsed_url = urllib.parse.urlparse(fandom_url)
-    base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+    base_url = _get_fandom_base_url(fandom_url)
     statistics_path = "wiki/Special:Statistics"
     return urllib.parse.urljoin(base_url + "/", statistics_path)
+
+
+def get_fandom_page_url(page_name: str, fandom_url: str) -> str:
+    """
+    Generates a Fandom wiki URL for a given character name.
+
+    Args:
+        page_name: The name of the page.
+        fandom_url: An URL from the Fandom wiki
+            (e.g., "https://asimov.fandom.com/wiki/Asimov_Wiki" or "https://asimov.fandom.com/").
+
+    Returns:
+        The constructed Fandom URL for the character.
+    """
+    base_url = _get_fandom_base_url(fandom_url)
+    return urllib.parse.urljoin(base_url + "/", page_name)
